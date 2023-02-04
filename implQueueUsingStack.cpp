3a379d882f3b4060218768e61a04e8c9762a39c2
Eyobed <88844574+eyuu1@users.noreply.github.com>
2023-02-04 16:30:26 +0300

    Create implQueueUsingStack.cpp

---
+++ implQueueUsingStack.cpp
@@ -0,0 +1,45 @@
+class MyQueue {
+public:
+stack<int>stk;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        stack<int>temp;
+        while(!stk.empty()){
+            temp.push(stk.top());
+            stk.pop();
+        }
+        stk.push(x);
+
+        while(!temp.empty()){
+            stk.push(temp.top());
+            temp.pop();
+        }
+    }
+    
+    int pop() {
+        int tem;
+        tem=stk.top();
+        stk.pop();
+        return tem;
+    }
+    
+    int peek() {
+        return stk.top();
+    }
+    
+    bool empty() {
+        return stk.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
