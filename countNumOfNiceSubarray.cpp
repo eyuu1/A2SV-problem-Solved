005a224a96248ae066ac6b1ccc693ac7a96a7718
Eyobed <88844574+eyuu1@users.noreply.github.com>
2023-02-11 22:03:57 +0300

    Create countNumOfNiceSubarray.cpp

---
+++ countNumOfNiceSubarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int i=0 , sum=0 ,count=0;
+        unordered_map<int,int>map;
+        while(i<nums.size()){
+            if(nums[i]%2 != 0) sum+=1;
+            if(sum == k) count++;
+             if(map.find(sum-k) != map.end()) count+=map[sum-k];
+        map[sum]++;
+         i++;
+        }
+        return count;
+    }
+};
